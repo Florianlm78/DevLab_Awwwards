69fb475414b305321cb8ddb9c6fcfe40ea40d033
Marvin Mensah <marvin.mensah@edu.devinci.fr>
2020-05-11 09:52:31 +0200

    menu-Li anim

--- index.js
+++ index.js
@@ -6,13 +6,40 @@ burgerMenu.onclick = function() {
     
     if (menuModal.style.display != 'block') {    
         menuModal.style.display = 'block';
-        menuModal.style.animation = 'rotate';
         burgerMenu.classList.toggle('open');
+    
+        TweenMax.to('#ml1', 2, {
+            y: "0",
+            opacity: "1",
+            ease: Expo.easeInOut
+        })
 
+        TweenMax.to('#ml2', 2, {
+            delay: "0.3",
+            y: "0",
+            opacity: "1",
+            ease: Expo.easeInOut
+        })
+
+        
+        TweenMax.to('#ml3', 2, {
+            delay: "0.7",
+            y: "0",
+            opacity: "1",
+            ease: Expo.easeInOut
+        })
+
+        
+        TweenMax.to('#ml4', 2, {
+            delay: "1",
+            y: "0",
+            opacity: "1",
+            ease: Expo.easeInOut
+        })
+
+        
     } else {
           menuModal.style.display = 'none';
           burgerMenu.classList.remove('open');
-          menuModal.style.animation = 'rotateInv';
-
 }
 }

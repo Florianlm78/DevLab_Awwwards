ce1ae167da9402ca4eb4e525da1f5f09150596b3
Florian <flo78480@hotmail.fr>
2020-05-18 18:00:03 +0200

    modif

--- index.js
+++ index.js
@@ -160,12 +160,3 @@ window.onload = () => {
   })
 }
 
-/*
-var item = document.getElementsByTagName('BODY')[0];
-
-window.addEventListener('wheel', function(e) {
-
-  if (e.deltaY > 0) item.scrollLeft += 100;
-  else item.scrollLeft -= 100;
-});
-*/
